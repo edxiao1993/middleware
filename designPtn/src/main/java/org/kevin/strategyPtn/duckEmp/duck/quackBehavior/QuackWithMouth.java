33edc32fef5b74f4a7288eec87764b863b61cb49
edxiao1993 <edxiao1993@gmail.com>
2021-10-30 12:41:44 +0800

    strategy pattern. done at October 30 - 2021

---
+++ designPtn/src/main/java/org/kevin/strategyPtn/duckEmp/duck/quackBehavior/QuackWithMouth.java
@@ -0,0 +1,11 @@
+package org.kevin.strategyPtn.duckEmp.duck.quackBehavior;
+
+/**
+ * @author Kevin.Z
+ * @version 2021/10/30
+ */
+public class QuackWithMouth implements QuackBehavior{
+    public void quack() {
+        System.out.println("muted......");
+    }
+}
